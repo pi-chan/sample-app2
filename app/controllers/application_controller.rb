0fab5ffc1d5ee86c90c5118d1683bafe374c7c3d
xoyip <xoyip@piyox.info>
2014-07-03 13:44:36 +0900

    ログイン後のページ遷移のためのstore_locationを呼びすぎないように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :store_location
 
   private
 
   def sign_in_required
+    store_location
     redirect_to new_user_session_url unless user_signed_in?
   end
 
